bdcdec125e3d8d4dcdf53e0b11c0316d384d36b4
asteria <189134833+asteriash@users.noreply.github.com>
2024-12-21 18:33:40 +0100

    Update screen timer

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@ let modes = {
   shortBreak: 5,
   longBreak: 15,
 }
+let pauseState = false;
 
 //Simply making a countdown, we'll introduce user interraction later.
 
@@ -11,11 +12,31 @@ function setTimer (mode){
   console.log(`Timer set for: ${duration} at ${mode}`); // Log for debugging
   let timerID = setInterval(() => { // We're setting the interval value to timerID so we can suspend and clear it
     if(duration >= 0){
-      duration--;
-      console.log(`Time left: ${duration} seconds.`);
+      if(pauseState == false){
+        duration--;
+        writeTimer(duration);
+        console.log(`Time left: ${duration} seconds.`);
+      }
     }
     else{
       clearInterval(timerID); // If the timer has already finished, just clear the interval, freeing the worker.
     }
-  }, 1000)
+  }, 1000); // loop each second (this is in miliseconds)
+}
+
+function pauseTimer (){
+  // pauseTimer == false ? pauseTimer = true : pauseTimer = false;
+  // Considering we're not idiots, we can just flip the bool using !(bool)
+  pauseState = !pauseState; // For the context this was not the first thing that came to mind.
+  console.log("Attempting to pause timer.");
+}
+
+function writeTimer(timeLeft) {
+  // The idea is to write over the ID of the button called "countdown"
+  let ss =  Math.floor(timeLeft / 60)
+  let mm = (timeLeft % 60) < 10 ? '0' + timeLeft % 60 : timeLeft % 60 
+  let time = ss + ":" + mm;
+  // Since we're calculating the moduo of timeLeft, it's more than likely that it ends up being a single digit instead of two.
+  
+  document.getElementById("countdown").innerHTML = `${time}`
 }
